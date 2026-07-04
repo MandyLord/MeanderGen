266f9b46ffda9fbec178a345e5784dbe88ae0c37
MandyLord <mandy.lord@gmail.com>
2026-07-04 21:55:17 +0100

    Update test_svg_writer.py

--- tests/test_svg_writer.py
+++ tests/test_svg_writer.py
@@ -1,1 +1,10 @@
+from src.geometry import Point
+from src.path import Path
+from src.svg_writer import SVGWriter
+
+def test_svg():
+    p=Path()
+    p.add_point(Point(0,0))
+    p.add_point(Point(10,10))
+    assert "<svg" in SVGWriter.path_to_svg(p)
 
