840de37a175dde8c5fd766e39daddbd63f2d390c
MandyLord <mandy.lord@gmail.com>
2026-07-05 10:53:19 +0100

    Update first_wander.py

--- examples/first_wander.py
+++ examples/first_wander.py
@@ -1,6 +1,20 @@
 from src.geometry import Point
 from src.pathfinder import PathFinder
 from src.movement_models import Wander
-finder=PathFinder(Wander(),seed=42)
-path=finder.generate(Point(50,50),0,25)
-print(path.point_count())
+from src.svg_writer import SVGWriter
+
+finder = PathFinder(
+    movement_model=Wander(max_turn_degrees=4.0, memory=0.85),
+    step_length=5.0,
+    seed=42,
+)
+
+path = finder.generate(
+    start=Point(50, 50),
+    heading=0.0,
+    steps=250,
+)
+
+SVGWriter.write(path, "examples/first_wander.svg", 300, 300)
+print("Created examples/first_wander.svg")
+
