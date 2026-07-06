66018b18b5ef555d90f5da41fd5299295be277a5
MandyLord <mandy.lord@gmail.com>
2026-07-06 23:29:17 +0100

    Update pathfinder.py

--- src/pathfinder.py
+++ src/pathfinder.py
@@ -5,6 +5,7 @@ from .path import Path
 from .state import State
 from .edge_steering import EdgeSteering
 from .self_avoidance import SelfAvoidance
+from .flow_steering import FlowSteering
 
 class PathFinder:
     def __init__(self,movement_model,step_length=5.0,seed=None,boundary_guide=None):
@@ -17,7 +18,8 @@ class PathFinder:
                 avoid_radius=120,
                 max_turn=6,
                 strength=0.08,
-        ),
+            ),
+            FlowSteering(strength=0.15),
         ]
 
         if boundary_guide is not None:
@@ -28,13 +30,23 @@ class PathFinder:
         current=start
         h=heading
         path.add_point(current)
+        previous_turn = 0.0
         
         for i in range(steps):
-            state = State(current, h, i, path)
-            turn=self.model.next_turn(self.rng)
+            state = State(
+                current,
+                h,
+                i,
+                path,
+                previous_turn
+            )
+            raw_turn = self.model.next_turn(self.rng)
+            smoothed_turn = previous_turn * 0.7 + raw_turn * 0.3
+            turn = smoothed_turn
             for behaviour in self.steering_behaviours:
                 turn += behaviour.steering_adjustment(state)
-            
+
+            previous_turn = smoothed_turn
             h += turn
             current=Point(
                 current.x+self.step*cos(radians(h)),
